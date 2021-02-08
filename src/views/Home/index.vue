bf47e33fa18b2a3b55b12bac01d33b6f99ae64df
Jeamar Libres <jeamarlibres@Jeamars-MacBook-Pro.local>
2021-02-08 09:14:43 +0800

    updated nginx file

---
+++ src/views/Home/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="home-container">
+		<Loader v-if="loader.isShow" :message="loader.message"/>
+		<Header/>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	@import "./style.scss";
+</style>
+
+<script>
+	import Home from './function.vue'
+	export default Home
+</script>
